dd02f94384376be4758c37842dc3bc491b585b16
XiaJun <xiajun960425@163.com>
2020-02-21 00:07:21 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,39 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h2>{{ count }}</h2>
+    <button @click="incre">增加</button>
+    <button @click="decre(2)">减少2</button>
+    <button @click="increment">map增加</button>
+    <button @click="decrement({amount: 4})">map减少4</button>
+    <button @click="asyncIncre">2s后减少1</button>
   </div>
 </template>
 
 <script>
+import { mapState, mapMutations, mapActions } from "vuex";
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  computed:{
+    ...mapState(['count'])
+  },
+  methods: {
+    incre () {
+      this.$store.commit('increment')
+    },
+    decre (a) {
+      // this.$store.commit('decrement',{amount: a})
+      this.$store.commit({type:'decrement',amount: a})
+      
+    },
+    //映射,可以直接用了
+    ...mapMutations(['increment','decrement']),
+    //映射 action
+    ...mapActions(['asyncIncre'])
+
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 0
   },
   mutations: {
+    increment(state){
+      state.count++
+    },
+    decrement(state, payload){
+      state.count-=payload.amount
+    }
   },
   actions: {
+    // asyncIncre(context){
+    //   context.commit('increment')
+    // }
+    //es6解构
+    asyncIncre({commit}){
+      // 两秒后提交
+      setTimeout(()=>commit('increment'),2000)
+    }
   },
   modules: {
   }
