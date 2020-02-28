436f47463dadd156dab02d09366fe93ed6955cb4
XiaJun <xiajun960425@163.com>
2020-02-28 22:35:40 +0800

    scroll

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <Swipe></Swipe> -->
+    <Scroll :scrollX="true" class="wrapper" ref="wrapper">
+      <div class="content" ref="content">
+        <div class="item">1</div>
+        <div class="item">2</div>
+        <div class="item">3</div>
+        <div class="item">4</div>
+        <div class="item">5</div>
+        <div class="item">6</div>
+        <div class="item">7</div>
+        <div class="item">8</div>
+        <div class="item">9</div>
+        <div class="item">10</div>
+        <div class="item">11</div>
+        <div class="item">12</div>
+        <div class="item">13</div>
+        <div class="item">14</div>
+        <div class="item">15</div>
+        <div class="item">16</div>
+        <div class="item">17</div>
+        <div class="item">18</div>
+        <div class="item">19</div>
+        <div class="item">20</div>
+        <div class="item">21</div>
+        <div class="item">22</div>
+        <div class="item">23</div>
+        <div class="item">24</div>
+        <div class="item">25</div>
+        <div class="item">26</div>
+        <div class="item">27</div>
+        <div class="item">28</div>
+        <div class="item">29</div>
+        <div class="item">30</div>
+      </div>
+    </Scroll>
+    qidian
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import Swipe from './components/Swipe'
+import Scroll from './components/Scroll'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // Swipe,
+    Scroll
+  },
+  mounted () {
+    let childs = this.$refs.content.children
+    let wid =childs.length * 50
+    this.$refs.content.style.width = wid + 'px'
+    console.log(wid)
   }
 }
 </script>
@@ -24,5 +65,24 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+/* // 要点 */
+.wrapper {
+  width: 100%;
+  overflow: hidden;
+}
+.content {
+  display: flex;
+}
+.item {
+  width: 50px;
+  height: 50px;
+  text-align: center
+}
+.item:nth-child(2n+1){
+  background-color: pink;
+}
+.item:nth-child(2n){
+  background-color: greenyellow;
 }
 </style>
---
+++ src/components/Swipe.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="swipe" ref="root">
+        <img :src="item" v-for="(item, index) in images" :key="index" />
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            images: [
+                require('@/assets/img/u=1654317949,2302406594&fm=26&gp=0.jpg'),
+                require('@/assets/img/u=2891173003,2665386216&fm=26&gp=0.jpg'),
+                require('@/assets/img/u=3104162126,318055222&fm=26&gp=0.jpg'),
+                require('@/assets/img/u=3207789083,432998702&fm=26&gp=0.jpg'),
+                require('@/assets/img/u=3708451574,4143490021&fm=26&gp=0.jpg')
+            ]
+        }
+    },
+    mounted() {
+        let root = this.$refs.root.children
+        let wid = 60
+        root[1].classList.add('active')
+        let index = 1
+        // setInterval(()=>{
+            
+        //     if (index !== root.length-1) {
+        //         index++
+                
+        //     } else {
+        //         index = 0
+        //     }
+        //     let next = index + 1 === root.length ? 0 : index + 1
+        //     root[index].classList.remove('active')
+        //     root[index].transform = `translate(${-wid}px)`
+        //     root[next].transform = `translate(${-wid}px)`
+        //     root[next].classList.add('active')
+        //     console.log(index, next)
+
+            
+        // },2000)
+        console.log(root)
+        console.log(wid)
+        
+    }
+}
+</script>
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+}
+.swipe {
+    position: relative;
+    width: 210px;
+    height: 120px;
+    overflow: hidden;
+}
+.swipe img {
+    /* position: absolute; */
+    width: 50px;
+    margin: 5px;
+    border-radius: 5px;
+    margin: 5px;
+    transition: 1s;
+}
+.active {
+    transform: scale(1.1);
+}
+</style>
